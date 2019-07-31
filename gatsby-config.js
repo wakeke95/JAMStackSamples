28f96f64f96666c8272555b8eafa49ddb83763f4
wakeke95 <53462155+wakeke95@users.noreply.github.com>
2019-07-31 16:14:08 +0800

    Commit changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,23 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
 
 module.exports = {
   /* Your site config here */
-}
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId,
+        accessToken
+      }
+    }
+  ]
+};
